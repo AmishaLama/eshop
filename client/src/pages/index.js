e691e7bcd9fc205bd8799a8ca8188006ba890c91
AmishaLama <amishalama5@gmail.com>
2023-07-17 22:04:36 +0545

    Basic Homepage Setup

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='body'> 
+    <img src='logo.jpg'/>
+    
+      <nav>
+      <img src='logo.jpg'/>
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">SERVICES</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+          <li>
+            <a href="#">FEEDBACK</a>
+          </li>
+        </ul>
+      </nav>
+      <section></section>
+    </div>
+  );
 }
+
 
 export default Home;
